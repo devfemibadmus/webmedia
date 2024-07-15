b6eadbfe9c825b00ce0e753fa92afd6bc57c4b9d
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-07-15 16:54:09 +0100

    :construction_worker: use cloudstorage to save uploaded media for 30mins, have the scraper class, video url can't be ext :dart: ract directly and gives back bcus of cookies which is use against bot etc so we using cloud storage :white_large_square:

--- main/app.py
+++ main/app.py
@@ -1,50 +1,85 @@
-from flask import Flask, render_template, request, jsonify, make_response, Response, send_file
+from flask import Flask, render_template, request, jsonify, session
+from flask_cors import cross_origin
+from engine.scraper import Scraper
+from google.cloud import storage
+from datetime import datetime
 from pathlib import Path
-import random, string
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 app = Flask(__name__, static_url_path='/static')
+app.config['SECRET_KEY'] = 'a5f4b6e9c7d1a8b2e0f3c9d2a8b1f4e5'
+application = app
 
-test_mode = True
+class CloudStorageManager:
+    def __init__(self):
+        self.storage_client = storage.Client.from_service_account_json(os.path.join(BASE_DIR, "mediasaver.json"))
+        self.bucket = self.storage_client.bucket('mediasaver')
 
-def testMode(video_url):
-    image_links = [
-        'https://cdnb.artstation.com/p/assets/images/images/034/948/193/large/aniket-gawande-01.jpg?1613671560',
-        'https://img.freepik.com/premium-photo/fun-cowboy-3d-illustration_183364-14701.jpg',
-        'https://storage.prompt-hunt.workers.dev/clflhwtzv000ll8079q60ff0s_1',
-        'https://imgv3.fotor.com/images/gallery/cartoon-character-generated-by-Fotor-ai-art-creator.jpg',
-        'https://img.freepik.com/premium-photo/little-boy-cowboy-with-cartoon-style-3d-rendering_646510-2373.jpg'
-    ]
-    return {'media_url': "https://videocdn.cdnpk.net/videos/dfedd6d9-20e8-40b5-8e93-9303f9f31930/horizontal/previews/videvo_watermarked/large.mp4", 'thumbnail_url': random.choice(image_links)}
+    def upload_file(self, file_folder, file_upload):
+        if file_upload:
+            blob = self.bucket.blob(file_folder + "/" + file_upload.filename)
+            blob.upload_from_file(file_upload)
+            signed_url = blob.generate_signed_url(expiration=1200, version='v4')
+            return signed_url
+        return False
 
+    def get_signed_url(self, file_folder, file_name, expiration=1200):
+        if file_name:
+            blob = self.bucket.blob(file_folder + "/" + file_name)
+            signed_url = blob.generate_signed_url(expiration=expiration, version='v4')
+            print("signed_url: ", signed_url)
+            return signed_url
+        return False
+
+    def delete_file(self, file_folder, file_name):
+        if file_name:
+            blob = self.bucket.blob(file_folder + "/" + file_name)
+            blob.delete()
+            return True
+        return False
+
+manager = CloudStorageManager()
 
 @app.route('/app/', methods=['POST'])
-def save_media():
-    video_url = request.form.get('url')
-    if test_mode:
-        response_data = testMode(video_url)
-    elif "facebook"in video_url:
-        response_data = facebook(video_url)
-    elif "youtube" in video_url:
-        response_data = youtube(video_url)
-    elif "instagram" in video_url:
-        response_data = instagram(video_url)
-    elif "tiktok" in video_url:
-        response_data = tiktok(video_url)
-
-    return jsonify(response_data)
-
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
+    now = datetime.now()
+    date = now.strftime("%m-%d-%Y")
+    time = now.strftime("%H:%M:%S")
+    file_folder = f"{date}/{time}"
+    if request.method == 'POST' and request.form.get('video_url'):
+        video_url = request.form.get('video_url')
+        scraper = Scraper()
+        response = scraper.get_video(file_folder, video_url)
+        return jsonify(response)
     return render_template("home.html")
+
+@app.route('/upload-video', methods=['POST'])
+@cross_origin()
+def upload_video():
+    if 'file' not in request.files:
+        session['error'] = "No file part"
+
+    file = request.files['file']
+    
+    if file.filename == '':
+        session['error'] = "No selected file"
+
+    file_folder = request.form.get('file_folder')
+    
+    video_url = manager.upload_file(file_folder, file)
+    
+    return jsonify({"message": "Video uploaded successfully!", "video_url": video_url})
+
+@app.route('/get-video-url/')
+def get_video_url():
+    video_url = manager.get_signed_url(session.get('file_folder'), session.get('file_name'))
+    return jsonify({"video_url": video_url})
 
 @app.route('/<path:path>')
 def catch_all(path):
     return render_template("home.html")
 
-
-"""
 if __name__ == '__main__':
     app.run(debug=True)
-"""
-application = app
