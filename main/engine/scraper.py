47efffeb79483a8b873138417fdf77b32a59470c
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-07-15 17:22:41 +0100

    :dart: still in developement mode so headless is false, here is what i do, wait you're a programmer right? read u will understand what i do...............in summary i love the response holding unto the reply rather using the session so on here we circulate. home post to server saver call scraper then scraper driver upload to server then server gives back response to scraper then scraper gives back respose to server then server gives back response to home then home launch the video_url. It's  :package: actually not that slow the scraper main post_video just launch the url then scrap the xpath and launch the video source in new tab and my js script the video to my server, then response circulate back to my home ajax code. open in new tab and not just requests.get video url bcus of bot security of website yeah they cookies the web page that launch the url so definitely there is no exact source to video url bcus we have CSRF_Token. So for now we have the tiktok we hoping on doing others :wrench:

---
+++ main/engine/scraper.py
@@ -0,0 +1,120 @@
+import time, re
+from flask import session
+from selenium import webdriver
+from engine.validator import Validator
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Scraper:
+    def __init__(self):
+        self.edge_options = Options()
+        self.edge_options.use_chromium = True
+        self.edge_options.headless = False
+        self.browser = webdriver.Edge(options=self.edge_options)
+        self.browser.delete_all_cookies()
+        self.browser.set_script_timeout(120)
+
+    def get_video(self, file_folder, video_url):
+        source = Validator.validate(video_url)
+        print("source: ", source)
+        if source == "TikTok":
+            return self.scrape_tiktok_video(file_folder, video_url)
+        elif source == "Instagram Post":
+            return self.scrape_instagram_post(file_folder, video_url)
+        elif source == "Facebook Reel":
+            return self.scrape_facebook_reel(file_folder, video_url)
+        elif source == "Facebook Video":
+            return self.scrape_facebook_video(file_folder, video_url)
+        else:
+            print(f"Unsupported video source: {source}")
+            return False
+
+    def scrape_tiktok_video(self, file_folder, tiktok_url):
+        video_element_xpaths = [
+            '//div[@class="css-8lv75m-DivBasicPlayerWrapper e1yey0rl2"]/div/video'
+        ]
+        video_id_match = re.search(r'video/(\d+)', tiktok_url)
+        video_attribute = 'src'
+        video_id = video_id_match.group(1) if video_id_match else None
+        response = {"error":False, "message":"response_data.message"}
+        for xpath in video_element_xpaths:
+            session.clear()
+            try:
+                response_data = self.post_video(file_folder=file_folder, video_url=tiktok_url, video_element_xpath=xpath, video_attribute=video_attribute, video_id=video_id)
+                if response_data.message == "Video uploaded successfully!":
+                    response = {"video_url":response_data.video_url, "success":True, "message":response_data.message}
+                else:
+                    response = {"error":False, "message":response_data.message}
+            except Exception as e:
+                response = f"Failed to scrape using XPath: {xpath}. Error: {e}"
+                print(f"Failed to scrape using XPath: {xpath}. Error: {e}")
+                continue
+        return response
+
+    def scrape_instagram_post(self, file_folder, instagram_url):
+        pass
+
+    def scrape_facebook_reel(self, file_folder, facebook_reel_url):
+        pass
+
+    def scrape_facebook_video(self, file_folder, facebook_video_url):
+        pass
+
+    def post_video(self, file_folder, video_url, video_element_xpath, video_attribute, video_id):
+        self.browser.get(video_url)
+
+        video_element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.XPATH, video_element_xpath))
+        )
+
+        video_url = video_element.get_attribute(video_attribute)
+        
+
+        js_code = f"""
+        return new Promise((resolve, reject) => {{
+        var videoElement = document.querySelector('video');
+        var videoUrl = videoElement.querySelector('source').src;
+        fetch(videoUrl)
+            .then(response => response.blob())
+            .then(blob => {{
+                var formData = new FormData();
+                formData.append('file', blob, '{video_id}.mp4');
+                formData.append('file_folder', '{file_folder}');
+                return fetch('http://localhost:5000/upload-video', {{
+                    method: 'POST',
+                    body: formData
+                }});
+            }})
+            .then(response => response.json())
+            .then(data => {{
+                resolve(data);
+            }})
+            .catch(error => {{
+                reject(error);
+            }});
+    }});
+    """
+
+        self.browser.execute_script(f"window.open('{video_url}', '_blank');")
+        time.sleep(5)
+
+        self.browser.switch_to.window(self.browser.window_handles[-1])
+        response_data = self.browser.execute_script(js_code)
+        # print("response_data: ", response_data)
+
+        self.browser.quit()
+        return response_data
+        
+
+
+"""
+# Cum test
+if __name__ == "__main__":
+    tiktok_url = 'https://www.tiktok.com/@oorsz/video/7380872550236048645?is_from_webapp=1&sender_device=pc'
+    scraper = Scraper()
+    scraper.scrape_tiktok_video(tiktok_url)
+"""
+
