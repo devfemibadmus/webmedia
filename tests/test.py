9f9444fa2f675f8a29f9500969152071853106aa
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-07-22 00:05:37 +0100

    :rotating_light: some late  :tada: date tests :x:

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,13 @@
-from flask import Flask, render_template, request, jsonify, session
+from datetime import datetime, timedelta
 
-print(jsonify("fuck"))
+initial_timestamp = datetime.now().isoformat()
+
+def is_6_seconds_late(initial_timestamp):
+    initial_time = datetime.fromisoformat(initial_timestamp)
+    current_time = datetime.now()
+    time_difference = current_time - initial_time
+    return time_difference >= timedelta(seconds=6)
+
+import time
+time.sleep(8)
+print(is_6_seconds_late(initial_timestamp))
