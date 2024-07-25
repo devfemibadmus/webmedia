ade7d73d3f9ad71556e3ad838f5c68a617a4a8c1
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-07-25 19:27:29 +0100

    :construction_worker: some test app.py :chart_with_upwards_trend:  confirm facebook :gem:

--- tests/run.py
+++ tests/run.py
@@ -1,19 +1,15 @@
-import requests
+import os, uuid, time, threading
 
-url = 'https://v16-webapp-prime.tiktok.com/video/tos/useast2a/tos-useast2a-pve-0068/oIK0jD5IfCFsBADfEQma4Jmtz2RimAyhEECBQP/?a=1988&bti=ODszNWYuMDE6&ch=0&cr=3&dr=0&lr=unwatermarked&cd=0%7C0%7C0%7C&cv=1&br=394&bt=197&cs=0&ds=6&ft=4fUEKMkj8Zmo0eiwG-4jVRTyppWrKsd.&mime_type=video_mp4&qs=5&rc=N2RlODc3PDczPDo4N2VnM0Bpajg4Om05cmpmdDMzNzczM0BiMTIyNV4xXy4xM2EuYF42YSNjLy1hMmRzcGFgLS1kMTZzcw%3D%3D&btag=e00090000&expire=1721689852&l=20240722170904F6395C5699D934330126&ply_type=2&policy=2&signature=cccb1d06d42f0d36dff3909e734afdda&tk=tt_chain_token'
-headers = {
-    'Referer': 'https://www.tiktok.com/',
-}
+binList =  [('TikTok/reel.vfx/7357012437419298090_0.jpg', 1721902080.7317343), ('TikTok/reel.vfx/7357012437419298090_1.jpg', 1721902080.7317343), ('TikTok/reel.vfx/7357012437419298090_2.jpg', 1721902080.7317343)]
 
-response = requests.get(url, headers=headers)
+bin_list =  ['TikTok/reel.vfx/7357012437419298090_0.jpg', 'TikTok/reel.vfx/7357012437419298090_0.jpg', 'TikTok/reel.vfx/7357012437419298090_0.jpg', 'TikTok/reel.vfx/7357012437419298090_0.jpg', 'TikTok/reel.vfx/7357012437419298090_1.jpg', 'TikTok/reel.vfx/7357012437419298090_0.jpg', 'TikTok/reel.vfx/7357012437419298090_0.jpg', 'TikTok/reel.vfx/7357012437419298090_0.jpg', 'TikTok/reel.vfx/7357012437419298090_2.jpg', ]
 
-# Check the status code and response content
-if response.status_code == 200:
-    print("Access is OK")
-    with open('video.mp4', 'wb') as file:
-        file.write(response.content)
-elif response.status_code == 403:
-    print("Access Denied")
-else:
-    print(f"Request failed with status code: {response.status_code}")
-    print(response.text)
+
+def add_to_bin_list():
+    timestamp = time.time() + 180
+    for bin in bin_list:
+        if bin not in [b[0] for b in binList]:
+            binList.append((bin, timestamp))
+    print("binList: ", binList)
+
+add_to_bin_list()
