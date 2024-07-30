1f30f2ba12d0daaeab9eef65354bf1fce66d0b35
Dev Femi Badmus <devfemibadmus@gmail.com>
2024-07-30 13:47:27 +0100

    :bento: release leaked quick :ambulance: one is just for test :alien:

--- quickOne/app.py
+++ quickOne/app.py
@@ -42,5 +42,5 @@ def catch_all(path):
     return render_template("home.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0')
 
---
+++ quickOne/platforms/leaked.py
@@ -0,0 +1,56 @@
+# Define the URL template
+
+video_tiktok = (
+    "https://www.tiktok.com/api/related/item_list/?WebIdLastTime=1722322797&aid=1988&app_language=en&app_name=tiktok_web"
+    "&browser_language=en-US&browser_name=Mozilla&browser_online=true&browser_platform=Win32&browser_version=5.0%20"
+    "%28Windows%20NT%2010.0%3B%20Win64%3B%20x64%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome"
+    "%2F126.0.0.0%20Safari%2F537.36%20Edg%2F126.0.0.0&channel=tiktok_web&clientABVersions=70508271%2C72213608%2C72313476"
+    "%2C72406134%2C72422413%2C72430804%2C72444800%2C72454782%2C72479966%2C72502691%2C72508984%2C72527745%2C72587086"
+    "%2C70405643%2C71057832%2C71200802%2C72445639&cookie_enabled=true&count=16&coverFormat=2&cursor=0&data_collection_enabled=false"
+    "&device_id=7397319992418551302&device_platform=web_pc&focus_state=true&from_page=video&history_len=2&isNonPersonalized=false"
+    "&is_fullscreen=false&is_page_visible=true&itemID={item_id}&language=en&odinId=7397319731298616326&os=windows&priority_region="
+    "&referer=&region=NG&screen_height=1080&screen_width=1920&tz_name=Africa%2FLagos&user_is_login=false&webcast_language=en"
+)
+
+image_tiktok = (
+        "https://www.tiktok.com/api/related/item_list/?WebIdLastTime=1722076276&aid=1988&app_language=en&app_name=tiktok_web"
+        "&browser_language=en-US&browser_name=Mozilla&browser_online=true&browser_platform=Win32&browser_version=5.0%20"
+        "%28Windows%20NT%2010.0%3B%20Win64%3B%20x64%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome"
+        "%2F126.0.0.0%20Safari%2F537.36%20Edg%2F126.0.0.0&channel=tiktok_web&clientABVersions=70508271%2C72213608%2C72313476"
+        "%2C72406134%2C72408648%2C72430804%2C72444798%2C72454780%2C72478059%2C72479966%2C72502689%2C72508181%2C72508984"
+        "%2C72510487%2C72516935%2C72527746%2C70405643%2C71057832%2C71200802%2C72445639&cookie_enabled=true&count=16"
+        "&coverFormat=2&cursor=0&data_collection_enabled=false&device_id=7396261186336409094&device_platform=web_pc"
+        "&focus_state=true&from_page=video&history_len=3&isNonPersonalized=false&is_fullscreen=false&is_page_visible=true"
+        "&itemID={item_id}&language=en&odinId=7396260937399845894&os=windows&priority_region=&referer=&region=NG"
+        "&screen_height=1080&screen_width=1920&tz_name=Africa%2FLagos&user_is_login=false&webcast_language=en"
+)
+
+
+instagram_data = {
+    'av': '0',
+    '__d': 'www',
+    '__user': '0',
+    '__a': '1',
+    '__req': '3',
+    '__hs': '19933.HYP:instagram_web_pkg.2.1..0.0',
+    'dpr': '1',
+    '__ccg': 'UNKNOWN',
+    '__rev': '1015220271',
+    #'__s': 'cez0hp:jdr2fx:xjjfjb',
+    #'__hsi': '7397101657472083388',
+    #'__dyn': '7xeUjG1mxu1syUbFp41twpUnwgU7SbzEdF8aUco2qwJw5ux609vCwjE1xoswaq0yE6ucw5Mx62G5UswoEcE7O2l0Fwqo31w9O1TwQzXwae4UaEW2G0AEco5G0zK5o4q3y1Sx-0iS2S3qazo7u3C2u2J0bS1LwTwKG1pg2fwxyo6O1FwlEcUed6goK2O4UrAwCAxW6Uf9EO2e2e1ew',
+    #'__csr': 'hk27Na4iF9bEgFqBIHlmGCGXiZ6F4JbKiGHhFWmVUmjDybykKi8gGfCCox1aucih9oXiAKUGdgLiBVC4L_ACGtaiaHhELUWq9BUF1Oa89FrUnyESrBDGl29UjApoGbx6qp5xCjyHAyE018Flg6OS0Ou0eJwXDyro6y1yg8P0Iw7kw0EgAgR0au72Gp2p46E2aq9o1zUeO3Cipr6042wprBkOzQFFwC5K1xzUCi9waqp5gCm1hg0ghypA9Bw0K6w0lbo',
+    '__comet_req': '7',
+    'lsd': 'AVqipa-__e8',
+    'jazoest': '2970',
+    '__spin_r': '1015220271',
+    '__spin_b': 'trunk',
+    '__spin_t': '1722271940',
+    'fb_api_caller_class': 'RelayModern',
+    'fb_api_req_friendly_name': 'PolarisPostActionLoadPostQueryQuery',
+    'variables': '{"shortcode":"item_id","fetch_comment_count":40,"parent_comment_count":24,"child_comment_count":3,"fetch_like_count":10,"fetch_tagged_user_count":null,"fetch_preview_comment_count":2,"has_threaded_comments":true,"hoisted_comment_id":null,"hoisted_reply_id":null}',
+    'server_timestamps': 'true',
+    'doc_id': '25531498899829322'
+}
+instagram_url = "https://www.instagram.com/graphql/query"
+
