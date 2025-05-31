6727c9ba86a31371b2e00f9ea268a6ce3ea34592
Femi Badmus <devfemibadmus@gmail.com>
2025-05-31 08:22:50 +0100

    debug

--- platforms/instagram.py
+++ platforms/instagram.py
@@ -150,7 +150,9 @@ class Instagram:
 
             instagram_data['variables'] = instagram_data['variables'].replace('item_id', item_id)
             # print(instagram_data)
-            response = requests.post(self.graphql, data=instagram_data, headers=self.headers) # , cookies=cookies
+            session = requests.Session()
+            session.cookies.clear()
+            response = session.post(self.graphql, data=instagram_data, headers=self.headers) # , cookies=cookies
             print(response.text)
 
             data = response.json()
